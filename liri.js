96ed900659f7874b339213d13a985caace821b17
Curtis Charles <ccharles2388@gmail.com>
2019-10-11 14:24:22 -0400

    added var moment

--- liri.js
+++ liri.js
@@ -3,7 +3,7 @@ var keys = require("./keys.js");
 var Spotify = require("node-spotify-api");
 var axios = require("axios");
 var inquirer = require("inquirer");
-// var moment = require("moment");
+var moment = require("moment");
 var spotify = new Spotify(keys.spotify);
 console.log(spotify);
 var artist = "john legend"
