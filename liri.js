7d0e70eb58013bb85dd670ada26630860bc3b4c1
Curtis Charles <ccharles2388@gmail.com>
2019-10-11 14:22:12 -0400

    logic for bands in town api

--- liri.js
+++ liri.js
@@ -1,5 +1,26 @@
 require("dotenv").config();
 var keys = require("./keys.js");
 var Spotify = require("node-spotify-api");
+var axios = require("axios");
+var inquirer = require("inquirer");
+// var moment = require("moment");
 var spotify = new Spotify(keys.spotify);
 console.log(spotify);
+var artist = "john legend"
+
+
+ // Constructing a queryURL using the artist name
+ var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+ 
+ axios.get(queryURL)
+ // After data comes back from the request
+ .then(function (response) {
+     console.log(queryURL);
+
+     var results = response.data;
+     for (var i = 0; i < results.length; i++) {
+        var event = results[i];
+        console.log(event);
+             }       
+ })
